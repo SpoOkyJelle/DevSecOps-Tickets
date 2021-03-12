827eb5e7380d11dfccc920d05c167b60219e71e9
Jelle Kootwijk <47365481+SpoOkyJelle@users.noreply.github.com>
2021-03-12 11:06:23 +0100

    Added add ticket
    - add ticket works
    - total tickets works

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flaskext.mysql import MySQL
+
+import datetime
 
 app = Flask(__name__)
 
@@ -23,6 +25,23 @@ def tickets():
     data='error with database'
 
   return render_template('tickets.html', data=data)
+
+@app.route("/addTicket", methods = ['POST'])
+def addTicket():
+  conn = mysql.connect()
+  cursor = conn.cursor()
+
+  cursor.execute("SELECT * FROM `tickets`")
+  data = cursor.fetchall()
+
+  ticketname = request.form['ticketname']
+  ticketdesc = request.form['ticketdesc']
+
+  sql = "INSERT INTO `tickets` (`ticket_name`, `ticket_description`, `created_at`) VALUES (%s, %s, %s)"
+  cursor.execute(sql, (ticketname, ticketdesc, datetime.datetime.now(),))
+
+  return render_template('tickets.html', data=data)
+  
 
 if __name__ == "__main__":
   app.run(debug=True)
