1fad541f254f143e31be11c71a492afd6a691d94
Jelle Kootwijk <47365481+SpoOkyJelle@users.noreply.github.com>
2021-03-12 10:34:30 +0100

    Added database link
    - database link
    - stylesheet

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+from flaskext.mysql import MySQL
+
 app = Flask(__name__)
 
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'python_tickets'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+
 @app.route("/")
-def hello():
-  return "Hello World!"
+def tickets():
+  try: 
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM `tickets`")
+    data = cursor.fetchall()
+  except:
+    print('error with database')
+    data='error with database'
+
+  return render_template('tickets.html', data=data)
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
