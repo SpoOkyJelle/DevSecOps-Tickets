b025cee66ac565d0d9b2be71066aa9da3251fbf2
Jelle Kootwijk <47365481+SpoOkyJelle@users.noreply.github.com>
2021-03-31 13:20:13 +0200

    Update python_tickets.sql

--- python_tickets.sql
+++ python_tickets.sql
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `tickets` (
   `solved` tinyint(1) DEFAULT NULL,
   `created_at` datetime(6) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `tickets`
